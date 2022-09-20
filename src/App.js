b138067064755af68377201b8e009a5253118e55
MahadiMunna <mahadimunna.official@gmail.com>
2022-09-21 00:21:19 +0600

    country data loaded

--- src/App.js
+++ src/App.js
@@ -18,13 +18,25 @@ function LoadCountries(){
     .then(res=>res.json())
     .then(data=>setCountries(data))
   },[])
+
   return(
     <div>
       <h1>visiting every country in the world!!!</h1>
       <h3>Available countries: {countries.length}</h3>
+      {
+        countries.map(country =><Country name= {country.name.common} population ={country.population}></Country>)
+      }
     </div>
   )
 }
 
+function Country(props){
+  return(
+    <div>
+      <h2>Name: {props.name}</h2>
+      <h4>Population: {props.population}</h4>
+    </div>
+  )
+}
 
 export default App;

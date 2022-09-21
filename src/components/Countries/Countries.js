2effdf130cce52675e18d81edb843a8d7f87debc
MahadiMunna <mahadimunna.official@gmail.com>
2022-09-21 21:57:08 +0600

    key add and use display style in parent component

--- src/components/Countries/Countries.js
+++ src/components/Countries/Countries.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Country from '../Country/Country';
+import './Countrries.css'
 
 const Countries = () => {
     const [countries, setCountries] = useState([])
@@ -12,9 +13,11 @@ const Countries = () => {
     return (
         <div>
             <h1>Hello from countries</h1>
+            <div  className='countries-container'>
             {
-                countries.map(country => <Country country={country}></Country>)
+                countries.map(country => <Country country={country} key={country.cca3}></Country>)
             }
+            </div>
         </div>
     );
 };
